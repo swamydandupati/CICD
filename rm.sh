e2bd6784cbf45444cf322fc46a0eb81f0c2c2b73
karthik <38027604+venkatakarthik94@users.noreply.github.com>
2021-08-11 11:32:12 +0530

    Update rm.sh

--- rm.sh
+++ rm.sh
@@ -4,7 +4,7 @@ echo "Stoping Docker containers"
 docker stop $(docker ps -q)
 elif [[ $(docker ps -a) == 0 ]]
 then
-echo "removing all docker containers"
+echo "removing all docker containers "
 docker rm -f $(docker ps -a)
 else
 sleep 1;
